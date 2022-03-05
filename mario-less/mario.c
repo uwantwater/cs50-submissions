b1765f2545c7a4664615844854e262641274f651
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-05 11:17:51 +0000

    Sat, Mar 5, 2022, 11:17 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int block_height(int n)
 {
-    
+    for 
+}
+
+int main(void)
+{
+    int n = get_int("Height?\n")
+    block_height(n)
 }
