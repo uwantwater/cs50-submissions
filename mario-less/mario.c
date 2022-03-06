de0a7e3e7b0cf59121218a363fb624512b1fff23
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-06 03:38:53 +0000

    Sun, Mar 6, 2022, 3:38 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -7,12 +7,13 @@ void block_height(int n)
     for (int i = 0; i < n; i++)
     {
         int k = (n - 2) - i;
-        //generating required no. of spaces to right align the 
+        //generating required no. of spaces to right align the hashes
         while (k >= 0)
         {
             printf(" ");
             k = k - 1;
         }
+        //for no. of hashes in one row
         for (int j = 0; j <= i; j++)
         {
             printf("#");
@@ -24,10 +25,12 @@ void block_height(int n)
 int main(void)
 {
     int n;
+    //requiring input to be positive numbers between 0 and 9
     do
     {
         n = get_int("Height?\n");
     }
     while (n < 1 || n > 8);
+    //calling the function
     block_height(n);
 }
