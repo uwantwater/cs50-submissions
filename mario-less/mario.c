b51750d92925ec0601bd9b6e4c27887dd1107e49
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-06 03:33:49 +0000

    Sun, Mar 6, 2022, 3:33 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -5,13 +5,13 @@ void block_height(int n)
 {
     for (int i = 0; i < n; i++)
     {
-            int k = (n-2) - i;
-            while (k >= 0)
-            {
-                printf(" ");
-                k = k - 1;
-            }
-            for (int j = 0; j <= i; j++)
+        int k = (n-2) - i;
+        while (k >= 0)
+        {
+            printf(" ");
+            k = k - 1;
+        }
+        for (int j = 0; j <= i; j++)
             {
             printf("#");
             }
