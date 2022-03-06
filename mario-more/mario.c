232212dc2b325e4e6dc829bd8d4513f5c27f31b7
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-06 03:48:48 +0000

    Sun, Mar 6, 2022, 3:48 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,41 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+void block_height(int n)
 {
-    
+    //for column
+    for (int i = 0; i < n; i++)
+    {
+        int k = (n - 2) - i;
+        //generating required no. of spaces to right align the hashes
+        while (k >= 0)
+        {
+            printf(" ");
+            k = k - 1;
+        }
+        //for no. of hashes in one row
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf(" ");
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
+int main(void)
+{
+    int n;
+    //requiring input to be positive numbers between 0 and 9
+    do
+    {
+        n = get_int("Height?\n");
+    }
+    while (n < 1 || n > 8);
+    //calling the function
+    block_height(n);
 }
