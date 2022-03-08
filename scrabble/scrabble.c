2957c6ccc3c4fa694f573c7e46dc45df44b463f6
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-08 09:53:20 +0000

    Tue, Mar 8, 2022, 9:53 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -24,8 +24,8 @@ int main(void)
 
 int compute_score(string word)
 {
-    int score;
-    for (int i = 0; i <= strlen(word); i++)
+    int score = 0;
+    for (int i = 0; i < strlen(word); i++)
     {
         score = score + (toupper(word[i]) - 64);
     }
