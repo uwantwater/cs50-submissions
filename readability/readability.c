052900175a5cba73a3d3e55f4332c0b8cc0c4e7d
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-10 09:06:05 +0000

    Thu, Mar 10, 2022, 9:06 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,40 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
-int main(void) 
+int letters(string text);
+int letterCount = 0;
+
+int sentences(string text);
+int sentenceCount = 0;
+
+int words(string text);
+int wordCount = 0;
+
+int main(void)
 {
+    string inputText = get_string("Text: ");
+    int no = letters(inputText);
+    printf("%i\n", no);
+}
 
+int letters(string text)
+{
+    for (int i = 0, n = strlen(text); i <= n; i++)
+    {
+        if (toupper(text[i]) >= 'A' && toupper(text[i]) <= 'Z')
+        {
+            letterCount = letterCount + 1;
+        }
+    }
+    return letterCount;
+}
+
+int sentences(string text)
+{
+    for (int i = 0, n = strlen(text); i <= n; i++)
+    {
+        if (text[i] =)
+    }
 }
