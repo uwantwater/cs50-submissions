36adcc70268d9f5b4e2810fcf1b68678a63be50a
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-11 16:08:49 +0000

    Fri, Mar 11, 2022, 4:08 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,10 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
     int key = atoi(argv[1]);
     string plaintext[] = get_string("plaintext:  ")
-    printf("%s\n", plaintext);
+    printf("%s\n", strcat(argv[1], plaintext));
 }
