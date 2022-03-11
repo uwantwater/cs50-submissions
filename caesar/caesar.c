28d7485b158cee63d671919a0418ac3a79f1bb5d
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-11 17:51:31 +0000

    Fri, Mar 11, 2022, 5:51 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -15,22 +15,16 @@ int main(int argc, string argv[])
     }
     //get required string that is supposed to be ciphered from user
     string plaintext = get_string("plaintext:  ");
-    string ciphered = cipherblock(plaintext);
+    string ciphered = cipherblock(plaintext, argv[1]);
     printf("ciphertext: %s", ciphered);
 }
 
-string cipherblock(string text)
+string cipherblock(string text, int key)
 {
     char ciphertext[64];
     char output[64];
     for (int i = 0, n = strlen(text); i <= n; i++)
     {
-        //convert given key from string type to integer type
-        int key = atoi(argv[1]);
-        if (key > 26)
-        {
-            key = key - 26;
-        }
         if (toupper(text[i]) >= 'A' && toupper(text[i]) <= 'Z')
         {
             if (toupper((text[i] + key)) > 'Z')
