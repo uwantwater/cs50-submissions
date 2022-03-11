fc7f77e70430853760aa775cc6727251790aa962
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-11 17:05:00 +0000

    Fri, Mar 11, 2022, 5:05 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -9,15 +9,15 @@ int main(int argc, string argv[])
     //convert given key from string type to integer type
     int key = atoi(argv[1]);
     string plaintext = get_string("plaintext:  ");
-    char ciphertext[50];
-    string output = "";
+    char ciphertext[64];
+    char output[64];
     for (int i = 0, n = strlen(plaintext); i <= n; i++)
     {
         if (toupper(plaintext[i]) >= 'A' && toupper(plaintext[i]) <= 'Z')
         {
             sprintf(ciphertext, "%c", plaintext[i]+ key);
-            strcat(output, (string) ciphertext);
+            strcat(output, ciphertext);
         }
     }
-    printf("%s\n", output);
+    printf("ciphertext: %s\n", output);
 }
