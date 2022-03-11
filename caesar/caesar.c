79de111b93ea5d1d77c3266879db7561628029d2
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-11 17:37:59 +0000

    Fri, Mar 11, 2022, 5:37 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <ctype.h>
 
+string cipherblock(string text);
+
 int main(int argc, string argv[])
 {
     if (argc != 2)
@@ -11,12 +13,14 @@ int main(int argc, string argv[])
         printf("Usage : ./caesar key");
         return 1;
     }
-
-    //get required string that is supposed to be ciphered from user
     string plaintext = get_string("plaintext:  ");
+}
+
+string cipherblock(string text)
+    //get required string that is supposed to be ciphered from user
     char ciphertext[64];
     char output[64];
-    for (int i = 0, n = strlen(plaintext); i <= n; i++)
+    for (int i = 0, n = strlen(text); i <= n; i++)
     {
         //convert given key from string type to integer type
         int key = atoi(argv[1]);
@@ -24,29 +28,29 @@ int main(int argc, string argv[])
         {
             key = key - 26;
         }
-        if (toupper(plaintext[i]) >= 'A' && toupper(plaintext[i]) <= 'Z')
+        if (toupper(text[i]) >= 'A' && toupper(text[i]) <= 'Z')
         {
-            if (toupper((plaintext[i] + key)) > 'Z')
+            if (toupper((text[i] + key)) > 'Z')
             {
                 key = key - 26;
                 //cipher character and store it in variable ciphertext
-                sprintf(ciphertext, "%c", plaintext[i] + key);
+                sprintf(ciphertext, "%c", text[i] + key);
                 //concatenate required output and ciphered character
                 strcat(output, ciphertext);
             }
             else
             {
                 //cipher character and store it in variable ciphertext
-                sprintf(ciphertext, "%c", plaintext[i] + key);
+                sprintf(ciphertext, "%c", text[i] + key);
                 //concatenate required output and ciphered character
                 strcat(output, ciphertext);
             }
         }
         else
         {
-            sprintf(ciphertext, "%c", plaintext[i]);
+            sprintf(ciphertext, "%c", text[i]);
             strcat(output, ciphertext);
         }
     }
-    printf("ciphertext: %s\n", output);
+    return output;
 }
