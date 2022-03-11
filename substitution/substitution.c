ad1c1bba4ae6b81b569a34d9f20dad43302d40cf
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-11 10:00:53 +0000

    Fri, Mar 11, 2022, 10:00 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -9,17 +9,18 @@ int index_of_character(char c);
 //take cipher key from command line input
 int main(int argc, string argv[])
 {
-    int index = index_of_character(char)argv[1]);
-    printf("index in alphabet is: %i" index);
+    int index = index_of_character((char) argv[1]);
+    printf("index in alphabet is: %i", index);
 }
 
 int index_of_character(char c)
 {
-    for (int j = 0,int k = 26; j <= k; j++)
+    for (int j = 0, k = 26; j <= k; j++)
     {
-        if (alphabet[j] == c)
+        if (alphabets[j] == c)
         {
             return j;
             break;
         }
     }
+}
