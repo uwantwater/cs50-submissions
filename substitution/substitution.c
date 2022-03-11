885b420300ec62391b8a43dba1c387460fcea7b4
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-11 10:04:23 +0000

    Fri, Mar 11, 2022, 10:04 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,26 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <string.h>
 
-string alphabets = "abcdefghijklmnopqrstuvwxyz";
-
-int index_of_character(char c);
-
-//take cipher key from command line input
 int main(int argc, string argv[])
 {
-    int index = index_of_character((char) argv[1]);
-    printf("index in alphabet is: %i", index);
-}
-
-int index_of_character(char c)
-{
-    for (int j = 0, k = 26; j <= k; j++)
-    {
-        if (alphabets[j] == c)
-        {
-            break;
-        }
-        return j;
-    }
+    string plaintext = getstring("plaintext: ");
+    
 }
