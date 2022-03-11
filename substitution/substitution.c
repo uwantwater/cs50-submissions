0f040cd4572695d1d0070d09775387ff8c8b43a9
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-11 08:58:54 +0000

    Fri, Mar 11, 2022, 8:58 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,9 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int cipher(string text);
+
 int main(int argc, string argv[])
 {
-    string plaintext = getstring("plaintext: ");
-
-    printf("")
+    string plaintext = getstring("plaintext:  ");
+    string ciphertext = cipher()
+    printf("ciphertext: %s", ciphertext);
 }
