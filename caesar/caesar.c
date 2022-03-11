d028509544abb5ca2448ea118016b41be50e14d5
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-11 16:18:26 +0000

    Fri, Mar 11, 2022, 4:18 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -2,15 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
     int key = atoi(argv[1]);
     string plaintext = get_string("plaintext:  ");
     string ciphertext = "";
-    for (i = 0, n = strlen(plaintext); i <= n; i++)
+    for (int i = 0, n = strlen(plaintext); i <= n; i++)
     {
-        if (plaintext[i] )
+        if (toupper(plaintext[i]) >= 'A' && toupper(plaintext[i]) <= 'Z')
+        {
+            ciphertext = strcat(ciphertext, plaintext[i] + key)
+        }
     }
-    printf("%s\n", strcat(argv[1], plaintext));
+    printf("%s\n", ciphertext);
 }
