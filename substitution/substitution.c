92f6eec92b5b6c3ecb3922f026c64fd73e3081d5
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-11 10:47:20 +0000

    Fri, Mar 11, 2022, 10:47 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,8 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
     string plaintext = get_string("plaintext: ");
-    string text = 
+    string text = strcat(argv, plaintext);
+    printf("%s\n", text);
 }
