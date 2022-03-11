9364c0918c4d1b9240ce9078694779c6f9a3ac14
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-11 16:14:05 +0000

    Fri, Mar 11, 2022, 4:14 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -7,5 +7,10 @@ int main(int argc, string argv[])
 {
     int key = atoi(argv[1]);
     string plaintext = get_string("plaintext:  ");
+    string ciphertext = "";
+    for (i = 0, n = strlen(plaintext); i <= n; i++)
+    {
+        if (plaintext[i] )
+    }
     printf("%s\n", strcat(argv[1], plaintext));
 }
