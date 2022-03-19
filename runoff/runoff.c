c204b8382450db451f71d714869b3a467f4e72ce
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-19 14:46:48 +0000

    Sat, Mar 19, 2022, 2:46 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
@@ -127,11 +128,12 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    for (int i = 0, i < candidate_count, i++)
+    for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(candidates.name[i], name) == 0)
+        if (strcmp(candidates[i].name, name) == 0)
         {
             preferences[voter][rank] = i;
+            return true;
         }
     }
     return false;
