2a2866c784e001743c08d3480f56122700b8aed1
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-19 14:58:30 +0000

    Sat, Mar 19, 2022, 2:58 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -142,7 +142,13 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    
+    for (int i = 0; i < voter_count; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            
+        }
+    }
     return;
 }
 
