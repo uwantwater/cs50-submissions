31e35e149fd0f508d14d5a2b97e9c8eccd2525ac
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-19 15:16:14 +0000

    Sat, Mar 19, 2022, 3:16 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -162,7 +162,14 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > (voter_count/2))
+        {
+            printf("%s\n", candidates[i].name);
+            return true;
+        }
+    }
     return false;
 }
 
