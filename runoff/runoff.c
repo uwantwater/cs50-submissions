3de84151ae719a637a484c70c37045d038d89eb6
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-03-19 14:43:55 +0000

    Sat, Mar 19, 2022, 2:43 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,7 +130,9 @@ bool vote(int voter, int rank, string name)
     for (int i = 0, i < candidate_count, i++)
     {
         if (strcmp(candidate.name[i], name) == 0)
-        
+        {
+            preferences[voter][rank] = 
+        }
     }
     return false;
 }
