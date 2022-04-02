ceb62d0d751805925ce8ab6fd74b96f93b47684b
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-04-02 11:46:09 +0000

    Sat, Apr 2, 2022, 11:46 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -39,7 +39,10 @@ int main(int argc, char *argv[])
     fwrite(header, sizeof(header), 1, output);
     // TODO: Read samples from input file and write updated data to output file
     typedef int16_t buffer;
-    
+    while (fread(buffer, sizeof(int16_t), 1, input))
+    {
+        
+    }
     // Close files
     fclose(input);
     fclose(output);
