1d65356440228e2680bc917ff788bd851d5eb51d
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-04-02 11:04:35 +0000

    Sat, Apr 2, 2022, 11:04 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,7 +34,11 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    
+    fread(header, )
+    for (i = 0; i < HEADER_SIZE; i++)
+    {
+
+    }
     // TODO: Read samples from input file and write updated data to output file
 
     // Close files
