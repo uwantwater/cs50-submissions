9ad1160a510b981468a5b4b90389d3d5490473a8
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-04-03 13:32:49 +0000

    Sun, Apr 3, 2022, 1:32 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -4,12 +4,26 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    //initialize required variables
     int average = 0;
+    int red = 0;
+    int green = 0;
+    int blue = 0;
+    //loop for columns
     for (i = 0, i < height, i++)
     {
+        //loop for rows
         for (j = 0, j < width, j++)
         {
-            average = image[i][j].
+            red = image[i][j].rgbtRed;
+            green = image[i][j].rgbtGreen;
+            blue = image[i][j].rgbtBlue;
+            //taking the average of the 3 RGB values
+            average = round((red+green+blue)/3.0);
+            //substituting the average values to the pixel
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
         }
     }
     return;
@@ -18,6 +32,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    
     return;
 }
 
