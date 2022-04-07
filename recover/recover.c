3503975ebd5604f03bd18460a71bc07b7a69dffa
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-04-07 10:07:04 +0000

    Thu, Apr 7, 2022, 10:07 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -14,6 +14,46 @@ int main(int argc, char *argv[])
         printf("Could not open file.\n");
         return 1;
     }
-    
+    FILE *img = NULL;
+
+    BYTE buffer[512];
+    char filename[8];
+    int counter = 0;
+    while (fread(buffer, sizeof(BYTE), 512, file) == 512)
+    {
+        // checks if start of img in buffer
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            // if start of img and first image i.e. counter ==0
+            // then begins writing a new image
+            if (counter == 0)
+            {
+                sprintf(filename, "%03i.jpg", counter);
+                img = fopen(filename, "w");
+                fwrite(&buffer, sizeof(BYTE), 512, img);
+                counter += 1;
+            }
+            // if start of img but not first image then
+            // closes the image and begins writing new image
+            else if (counter > 0)
+            {
+                fclose(img);
+                sprintf(filename, "%03i.jpg", counter);
+                img = fopen(filename, "w");
+                fwrite(&buffer, sizeof(BYTE), 512, img);
+                counter += 1;
+            }
+        }
+        // if not start of new img
+        // then it keeps on writing the image
+        else if (counter > 0)
+        {
+            fwrite(&buffer, sizeof(BYTE), 512, img);
+        }
+
+    }
+
+    // Close file
     fclose(file);
+    fclose(img);
 }
