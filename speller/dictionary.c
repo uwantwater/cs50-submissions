fb12063f8a59637227353478cb3a00429ecf8c4d
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-05-29 04:27:58 +0000

    Sun, May 29, 2022, 4:27 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -37,7 +37,7 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
+    FILE *file = fopen(argv[1], "r");
     return false;
 }
 
