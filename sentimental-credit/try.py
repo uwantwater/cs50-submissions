7281a0393aca80ed81a13d4ce6a8f245bd06d2e4
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-05-31 11:32:30 +0000

    Tue, May 31, 2022, 11:32 AM +00:00

--- sentimental-credit/try.py
+++ sentimental-credit/try.py
@@ -1,4 +1,4 @@
-import functools
+from functools import reduce
 
 number = [1,2,3,4,5,6,7,8,9,10,11]
 
@@ -10,7 +10,7 @@ for i in range(len(number) - 1, 0, -2):
 for i in range(len(number), 0, -2):
     list2.append(i)
 list1 = list(map(lambda x : x * 2, list1))
-list1 = list(functools.reduce(lambda x,y : x+y, list1))
+list1 = list(reduce(lambda x , y : x + y, list1))
 
 print(list1)
 print(list2)
