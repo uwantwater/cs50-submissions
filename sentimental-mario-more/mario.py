97f679452e5653bf79a6a3f972b311c4b714c2f4
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-05-31 09:58:49 +0000

    Tue, May 31, 2022, 9:58 AM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -2,8 +2,13 @@ def main():
     height = 0
     while height > 8 or height <= 0:
         height = int(input("Height: "))
+        h = height / 2
     for i in range(height):
-        print(" " * height)
+        print(" " * (h-i), end='')
+        print("#" * i, end='')
+        print("  ", end='')
+        print("#" * i, end='')
+        print(" " * (h-i))
 
 if __name__ == '__main__':
     main()
