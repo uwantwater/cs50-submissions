73ad5d23b445efbe6ed9c6b5f2533a6a04cbfa43
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-05-31 07:29:31 +0000

    Tue, May 31, 2022, 7:29 AM +00:00

--- world-cup/try.py
+++ world-cup/try.py
@@ -4,7 +4,7 @@ teams = []
 file = open("2018m.csv", "r")
 reader = csv.DictReader(file)
 for row in reader:
-    team = row(team)
-    teams[team] = int(row(rating))
+    team = row["team"]
+    teams[team] = int(row["rating"])
 
 print(teams)
