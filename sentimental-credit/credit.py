65a8de04bafd50861ac580e3c8ac30d7d7942971
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-05-31 11:41:27 +0000

    Tue, May 31, 2022, 11:41 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,11 +1,13 @@
 import sys
+from functools import reduce
+from operator import add
 
 def main():
     card_num = input("Number: ")
     while card_num.isnumeric() == False:
         card_num = input("Number: ")
     if checksum(card_num) == True:
-
+        if
     else:
         sys.exit("INVALID\n")
 
@@ -17,7 +19,13 @@ def checksum(number):
     for i in range(len(number), 0, -2):
         list2.append(i)
     list1 = list(map(lambda x : x * 2, list1))
-    
+    list1 = reduce(add, list1)
+    list2 = reduce(add, list2)
+    sum = list1 + list2
+    if sum % 10 == 0:
+        return True
+    return False
+
 
 if __name__ == '__main__':
     main()
