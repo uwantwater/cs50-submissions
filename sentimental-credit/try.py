441178a40d28cdc3f7e212b4afedc0b085488a6d
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-05-31 14:02:07 +0000

    Tue, May 31, 2022, 2:02 PM +00:00

--- sentimental-credit/try.py
+++ sentimental-credit/try.py
@@ -3,8 +3,11 @@ from operator import add
 
 number = list(str(378282246310005))
 
-list1 = []
-list2 = []
+list1 = list(str(378282246310005))
+list2 = list(str(378282246310005))
 
-print(len(number))
+for i in range(0:len(number):2):
+    print(number[i])
+
+print(list1)
 print(list2)
