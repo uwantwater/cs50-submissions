7b3e63ff2466b293110eda597bf7f5618a0e7b90
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-05-31 12:17:48 +0000

    Tue, May 31, 2022, 12:17 PM +00:00

--- sentimental-credit/try.py
+++ sentimental-credit/try.py
@@ -7,10 +7,10 @@ number = list(map(int, number))
 list1 = []
 list2 = []
 
+for i in range(len(number) - 2, 0, -2):
+    list1.append(number[i])
 for i in range(len(number) - 1, 0, -2):
-    list1.append(i)
-for i in range(len(number), 0, -2):
-    list2.append(i)
+    list2.append(number[i])
 print(number)
 print(list1)
 print(list2)
