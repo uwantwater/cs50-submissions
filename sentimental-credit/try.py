e8cb33dbac44d0e936b71c7bfc35638b08d5f507
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-05-31 12:09:01 +0000

    Tue, May 31, 2022, 12:09 PM +00:00

--- sentimental-credit/try.py
+++ sentimental-credit/try.py
@@ -1,9 +1,24 @@
-number = 4003600000000014
+from functools import reduce
+from operator import add
 
-number = list(str(number))
+number = list(str([378282246310005]))
 
-num = ""
-number = num.join(number)
+list1 = []
+list2 = []
 
-print(type(number))
-print(number[0])
+for i in range(len(number) - 1, 0, -2):
+    list1.append(i)
+for i in range(len(number), 0, -2):
+    list2.append(i)
+print(list1)
+print(list2)
+list1 = list(map(lambda x : x * 2, list1))
+
+list2 = reduce(add, list2)
+list1 = reduce(add, list1)
+
+sum = list1 + list2
+if sum % 10 == 0:
+    print("True")
+else:
+    print(f"False {sum}")
