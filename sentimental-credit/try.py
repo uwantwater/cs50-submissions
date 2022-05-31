3b6262ac93e059769b307f8581f8773959502db4
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-05-31 12:11:27 +0000

    Tue, May 31, 2022, 12:11 PM +00:00

--- sentimental-credit/try.py
+++ sentimental-credit/try.py
@@ -1,7 +1,7 @@
 from functools import reduce
 from operator import add
 
-number = list(str([378282246310005]))
+number = list(str(378282246310005))
 
 list1 = []
 list2 = []
@@ -10,6 +10,7 @@ for i in range(len(number) - 1, 0, -2):
     list1.append(i)
 for i in range(len(number), 0, -2):
     list2.append(i)
+print(number)
 print(list1)
 print(list2)
 list1 = list(map(lambda x : x * 2, list1))
