3cc216bf1ec117f33c60a22b9fb62f6a3a0ce959
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-05-31 15:28:08 +0000

    Tue, May 31, 2022, 3:28 PM +00:00

--- sentimental-credit/try.py
+++ sentimental-credit/try.py
@@ -6,8 +6,9 @@ number = list(str(378282246310005))
 listm = list(str(378282246310005))
 listn = list(str(378282246310005))
 
-for i in range(0 : len(number) : 2):
-    print(number[i])
+del listm[-1]
+listm = listm[::-1]
+listm = listm[::2]
 
 print(listm)
 print(listn)
