bed05fe7db95841553e80fb9b61b54976f477256
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-05-31 15:31:24 +0000

    Tue, May 31, 2022, 3:31 PM +00:00

--- sentimental-credit/try.py
+++ sentimental-credit/try.py
@@ -7,8 +7,8 @@ listm = list(str(378282246310005))
 listn = list(str(378282246310005))
 
 del listm[-1]
-listm = listm[::-1]
-listm = listm[::2]
+for i in range(len(listm) - 1, 0, -2):
+    print(listm[i])
 
 print(listm)
 print(listn)
