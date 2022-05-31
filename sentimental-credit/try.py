c8cb85fc75a671e7fb794dcfb18a9ec1ce3a9225
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-05-31 14:03:24 +0000

    Tue, May 31, 2022, 2:03 PM +00:00

--- sentimental-credit/try.py
+++ sentimental-credit/try.py
@@ -6,7 +6,7 @@ number = list(str(378282246310005))
 list1 = list(str(378282246310005))
 list2 = list(str(378282246310005))
 
-for i in range(0:len(number):2):
+for i in range(0 : len(number) : 2):
     print(number[i])
 
 print(list1)
