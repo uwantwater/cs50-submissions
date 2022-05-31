bc0dcd888733a3dc20705cc37a7b723f61fd7972
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-05-31 10:47:55 +0000

    Tue, May 31, 2022, 10:47 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,7 +1,14 @@
+import sys
 def main():
     card_num = input("Number: ")
     while card_num.isnumeric() == False:
         card_num = input("Number: ")
+    if checksum(card_num) == True:
+
+    else:
+        sys.exit("INVALID\n")
+
+def checksum(number):
     
 
 
