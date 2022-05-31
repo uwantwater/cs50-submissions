d9d3bd43df5ba03110fe1a563fa3e280bbbac6b9
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-05-31 11:18:06 +0000

    Tue, May 31, 2022, 11:18 AM +00:00

--- sentimental-credit/try.py
+++ sentimental-credit/try.py
@@ -1,12 +1,12 @@
-number = [1,2,3,4,5,6,7,8,9]
+number = [1,2,3,4,5,6,7,8,9,10,11]
 
 list1 = []
 list2 = []
 
-for i in range(len(number), 0, -2):
-    list1.append(number[i])
-for i in range(len(number - 1), 0, -2):
-    list2.append(number[i])
+#for i in range(len(number), 0, -2):
+ #   list1.append(number[i])
+#for i in range(len(number - 1), 0, -2):
+ #   list2.append(number[i])
 
-print(list1)
-print(list2)
+print(number[::2])
+print(number[2::])
