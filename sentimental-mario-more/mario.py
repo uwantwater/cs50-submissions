ab8b28fe3bcc0903054166c8b0ca3e530a404a74
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-05-31 10:20:57 +0000

    Tue, May 31, 2022, 10:20 AM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,16 +1,23 @@
 def main():
     height = 0
+    # djdciuiuejnjsdkjsdk
     try:
         while height > 8 or height <= 0:
             height = int(input("Height: "))
     except ValueError:
         while height > 8 or height <= 0:
             height = int(input("Height: "))
-    for i in range(1,height+1):
+    # jdedoijqwedk
+    for i in range(1, height+1):
+        # ednowej0e23eoe
         print(" " * (height-i), end='')
+        # awkdo3jedcj
         print("#" * i, end='')
+        # weijdowew
         print("  ", end='')
+        # ijdwendewkkd
         print("#" * i)
+
 
 if __name__ == '__main__':
     main()
