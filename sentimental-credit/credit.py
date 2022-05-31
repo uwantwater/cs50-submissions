51a21e9b830a3589f459862d75e6948d2455578e
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-05-31 12:25:43 +0000

    Tue, May 31, 2022, 12:25 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -27,10 +27,10 @@ def checksum(number):
     list2 = []
 
     for i in range(len(number) - 1, 0, -2):
-        list1.append(i)
+        list1.append(number[i])
 
     for i in range(len(number), 0, -2):
-        list2.append(i)
+        list2.append(number[i])
 
     list1 = list(map(lambda x : x * 2, list1))
 
