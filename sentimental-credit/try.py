d0c98af83ce39bb5185c044ab311ffe2d448d756
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-05-31 13:01:57 +0000

    Tue, May 31, 2022, 1:01 PM +00:00

--- sentimental-credit/try.py
+++ sentimental-credit/try.py
@@ -12,7 +12,8 @@ list2 = []
 #for i in range(len(number) - 2, -1, -2):
 #    list2.append(number[i])
 
-list1 = number[0:]
+list1 = number[0:len(number)-1]
+list1 = list1[::2]
 list2 = number[2::]
 
 print(number)
