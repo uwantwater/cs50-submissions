dcdff2cc2529d392211b524d311a57a52b17a9d2
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-06-01 06:33:44 +0000

    Wed, Jun 1, 2022, 6:33 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,6 +5,24 @@ def main():
     l = letters(inputText)
     w = words(inputText)
     s = sentences(inputText)
+    # letters per hundred words
+    L = l / w * 100
+    # sentences per hundred words
+    S = s / w * 100
+    rawIndex = 0.0588 * L - 0.296 * S - 15.8
+    # rounding index to the nearest integer
+    index = round(rawIndex)
+    # check if index is less than one or greater then 15 or between one and 16
+    if index < 1:
+        print("Before Grade 1")
+    elif index >= 16:
+        print("Grade 16+")
+    else:
+        print("Grade")
+
+def letters(text):
+    
+
 
 if __name__ == '__main__':
     main()
