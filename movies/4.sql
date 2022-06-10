a82b71daea5da35c7e6eef99cc31cb4a9f80c077
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-06-10 11:23:05 +0000

    Fri, Jun 10, 2022, 11:23 AM +00:00

--- movies/4.sql
+++ movies/4.sql
@@ -1,1 +1,3 @@
-SELECT title FROM movies WHERE id=(SELECT movie_id FROM ratings WHERE rating=10.0);
+select count(movies.id) from movies
+join ratings on movies.id=ratings.movie_id
+and ratings.rating=10;
