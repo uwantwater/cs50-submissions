647fdbd3b02afafb88fb66a5a2b6c3dd70836d4b
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-06-12 04:29:37 +0000

    Sun, Jun 12, 2022, 4:29 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,3 +6,4 @@ SELECT name, passport_number FROM people WHERE license_plate IN (SELECT license_
 SELECT id, name FROM people WHERE id IN (SELECT person_id FROM bank_accounts JOIN atm_transactions ON bank_accounts.account_number=atm_transactions.account_number WHERE year=2021 AND transaction_type="withdraw"); --suspects2
 SELECT id, name, passport_number FROM people WHERE phone_number IN (SELECT phone_number FROM phone_calls WHERE year=2021 AND month=7 AND day=28 AND duration<60) ORDER BY name; --sus people bahahhaajajaj
 SELECT * FROM airports WHERE city="Fiftyville"; --to get id of fiftyville airport
+SELECT id, name, passport_number FROM people WHERE passport_number IN (SELECT passport_number FROM passengers JOIN flights on flights.id=passengers.flight_id WHERE year=2021 AND month=7 AND day=29 AND origin_airport_id=8) ORDER BY name; --sus people amog us 4 :(
