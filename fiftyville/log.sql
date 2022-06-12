754da556c84f24fff9e96eb88884f99eb81ca151
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-06-12 03:31:17 +0000

    Sun, Jun 12, 2022, 3:31 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,3 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-
+SELECT * FROM crime_scene_reports WHERE year=2021 AND month=7 AND day=28; --to get more details of the crime
+SELECT * FROM interviews WHERE year=2021 AND month=7 AND day=28; --to review the interviews for clues
