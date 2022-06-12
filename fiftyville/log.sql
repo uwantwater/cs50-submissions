a9e359a7b5ddf6e4a258416b62de0e61884b0a2b
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-06-12 04:06:40 +0000

    Sun, Jun 12, 2022, 4:06 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,4 +3,4 @@ SELECT * FROM crime_scene_reports WHERE year=2021 AND month=7 AND day=28; --to g
 SELECT * FROM interviews WHERE year=2021 AND month=7 AND day=28; --to review the interviews for clues
 SELECT * FROM bakery_security_logs WHERE year=2021 AND month=7 AND day=28 AND hour=10 AND minute BETWEEN 15 AND 25; --first witness mentions car leaving within 10 minutes after theft
 SELECT name, passport_number FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year=2021 AND month=7 AND day=28 AND hour=10 AND minute BETWEEN 15 AND 25); --suspects1
-SELECT person_id FROM bank_accounts JOIN atm_transactions ON bank_accounts.account_number=atm_transactions.account_number WHERE year=2021 AND transaction_type="withdraw";
+SELECT id, name FROM people WHERE id IN (SELECT person_id FROM bank_accounts JOIN atm_transactions ON bank_accounts.account_number=atm_transactions.account_number WHERE year=2021 AND transaction_type="withdraw"); --suspects2
