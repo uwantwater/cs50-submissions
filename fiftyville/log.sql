5aa6239a2545a6b55a07223145ab69d5a966c345
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-06-12 03:38:21 +0000

    Sun, Jun 12, 2022, 3:38 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT * FROM crime_scene_reports WHERE year=2021 AND month=7 AND day=28; --to get more details of the crime
 SELECT * FROM interviews WHERE year=2021 AND month=7 AND day=28; --to review the interviews for clues
+SELECT * FROM bakery_security_logs WHERE year=2021 AND month=7 AND day=28 AND hour=10 AND minute BETWEEN 15 AND 25; --first witness mentions car leaving 10 minutes after theft
