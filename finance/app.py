817a2bee44d41b78be77d53029e19229de290b35
uwantwater <97341546+uwantwater@users.noreply.github.com>
2022-07-13 00:17:15 +0000

    Wed, Jul 13, 2022, 12:17 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -117,7 +117,7 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+    return
 
 
 @app.route("/sell", methods=["GET", "POST"])
